f0a889f36070e72010cb6d6820204a78baffc92a
serhii <proektssd@gmail.com>
2022-10-21 14:47:25 +0300

    fix decorator

--- decorator.py
+++ decorator.py
@@ -5,8 +5,8 @@ def decorator(func):
         print("--- что-то ПЕРЕД функцией ---")
         func()
         print("--- что-то ПОСЛЕ функции ---")
-    
-    return wrapper  # возвращаем объект типа <class 'function'>
+
+    return wrapper
 
 
 # @decorator
@@ -16,25 +16,3 @@ def some_func():
 
 some_func = decorator(some_func)
 some_func()
-
-# --- что-то ПЕРЕД функцией ---
-# Вызов функции some_func
-# --- что-то ПОСЛЕ функции ---
-
-
-# def my_decorator(func):
-#     print("Я обычная функция")
-#     def wrapper():
-#         print("Я - функция, возвращаемая декоратором")
-#         func()
-#     return wrapper
-#
-# def lazy_function():
-#     print("zzzzzzzz")
-#
-# decorated_function = my_decorator(lazy_function)
-#
-# # @my_decorator
-# # def lazy_function():
-# #     print("zzzzzzzz")
-
