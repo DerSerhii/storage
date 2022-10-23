8284e307b737b0e5999b7e0002415c4cdb283bae
serhii <proektssd@gmail.com>
2022-10-23 20:00:03 +0300

    fix dict

--- dict.py
+++ dict.py
@@ -11,3 +11,5 @@ d2 = {'+7': 2345678901, '+4': 3456789012, '+5': 5678901234, '+12': 78901234}
 print(*sorted(d2.items(), key=lambda x: int(x[0])))
 print(*sorted(d2.values(), key=lambda x: int(x)))
 print(*sorted(d2.keys(), key=lambda x: int(x)))
+
+print({k: v for k, v in sorted(d2.items(), key=lambda x: (int(x[0][1:]), x[1]))})
